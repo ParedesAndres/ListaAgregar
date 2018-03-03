b8e99ed0e09b5437cd08344239f35f83dc02437d
ParedesAndres <35270955+ParedesAndres@users.noreply.github.com>
2018-03-02 21:00:06 -0500

    ClassRoom

--- app/src/main/java/com/andresparedes/listaagregar/Adaptador.java
+++ app/src/main/java/com/andresparedes/listaagregar/Adaptador.java
@@ -1,6 +1,11 @@
 package com.andresparedes.listaagregar;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +22,7 @@ import java.util.ArrayList;
 public class Adaptador extends BaseAdapter{
 
     ArrayList<Contacto> contactos;
+    ArrayList<Contacto> contactosDinamicos;
     Activity activity;
 
     TextView nombreyapellido, tv_telefono;
@@ -25,16 +31,17 @@ public class Adaptador extends BaseAdapter{
     public Adaptador(Activity activity){
         this.activity = activity;
         contactos = new ArrayList<>();
+        contactosDinamicos = new ArrayList<>();
     }
 
     @Override
     public int getCount() {
-        return contactos.size();
+        return contactosDinamicos.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return contactos.get(i);
+        return contactosDinamicos.get(i);
     }
 
     @Override
@@ -55,23 +62,55 @@ public class Adaptador extends BaseAdapter{
         btn_editar = (Button) v.findViewById(R.id.btn_editar);
         btn_eliminar = (Button) v.findViewById(R.id.btn_eliminar);
 
-        nombreyapellido.setText(contactos.get(i).getNombre());
-        tv_telefono.setText(contactos.get(i).getTelefono());
+        nombreyapellido.setText(contactosDinamicos.get(i).getNombre());
+        tv_telefono.setText(contactosDinamicos.get(i).getTelefono());
 
-        //Elimina los
+        //Elimina los contactos
         btn_eliminar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                contactosDinamicos.remove(i);
                 contactos.remove(i);
                 notifyDataSetChanged();
             }
         });
+
+       /* btn_editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });*/
+
+
 
         return v;
     }
 
     public void addContacto(Contacto nuevo){
         contactos.add(nuevo);
+        contactosDinamicos = (ArrayList<Contacto>) contactos.clone();
         notifyDataSetChanged();
+    }
+
+    public void buscarContacto(String busqueda){
+        for (int i = 0; i < contactosDinamicos.size();i++){
+            if (!(contactosDinamicos.get(i).getNombre().contains(busqueda) || contactosDinamicos.get(i).getTelefono().contains(busqueda))){
+                contactosDinamicos.remove(i);
+                i--;
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+    public void llamar(int position){
+        Contacto c = (Contacto) contactosDinamicos.get(position);
+        Intent i = new Intent(Intent.ACTION_CALL);
+        i.setData(Uri.parse("tel:" + c.getTelefono()));
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CALL_PHONE}, 11);
+        } else {
+            activity.startActivity(i);
+        }
     }
 }
